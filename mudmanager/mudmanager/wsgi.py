ef499209a1601ca0507e8e6a7b596b4508869651
Oliver-Strange <preston.burton@gmail.com>
2019-09-26 09:17:14 -0700

    tries changing the settings route in wsgi

--- mudmanager/mudmanager/wsgi.py
+++ mudmanager/mudmanager/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mudmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'mudmanager.mudmanager.settings')
 
 application = get_wsgi_application()
