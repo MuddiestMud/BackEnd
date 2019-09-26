ef8272bf31d86765904ea6737df143c91aff92a5
Oliver-Strange <preston.burton@gmail.com>
2019-09-26 09:28:01 -0700

    still trying

--- mudmanager/mudmanager/wsgi.py
+++ mudmanager/mudmanager/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'mudmanager.mudmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mudmanager.settings')
 
 application = get_wsgi_application()
